89c45b9909ba0eb4ea75331a3babe3eb62c887d6
AbdelhamedBelal <abdelhamiedb@gmail.com>
2024-10-19 00:10:49 +0300

    Basket Repository

---
+++ StoreWeb.Repository/Repositories/BasketRepository.cs
@@ -0,0 +1,44 @@
+﻿using StackExchange.Redis;
+using StoreWeb.Core.Entities.Basket;
+using StoreWeb.Core.Repositories.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace StoreWeb.Repository.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDatabase _database;
+
+        public BasketRepository(IConnectionMultiplexer redis)
+        {
+            _database=redis.GetDatabase();
+        }
+
+        public async Task<bool> DeleteBasketAsync(string basketId)
+        {
+            return await _database.KeyDeleteAsync(basketId);
+        }
+
+        public async Task<CustomerBasket?> GetBasketAsync(string basketId)
+        {
+            var basket = await _database.StringGetAsync(basketId);
+
+            return basket.IsNullOrEmpty ? null : JsonSerializer.Deserialize<CustomerBasket>(basket);
+        }
+
+        public async Task<CustomerBasket?> UpdateBasketAsync(CustomerBasket basket)
+        {
+            var createdOrUpdatedBasket = await _database.StringSetAsync(basket.Id, JsonSerializer.Serialize(basket), TimeSpan.FromDays(30));
+
+            if (createdOrUpdatedBasket is false) return null;
+
+            return await GetBasketAsync(basket.Id);
+
+        }
+    }
+}
