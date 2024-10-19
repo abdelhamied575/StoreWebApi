d8c1da95465b47647629d2d8f353b6f7ee0d278e
AbdelhamedBelal <abdelhamiedb@gmail.com>
2024-10-20 00:31:09 +0300

    Cashe Service

--- StoreWebApi/MiddleWares/ExceptionMiddleWare.cs
+++ StoreWebApi/MiddleWares/ExceptionMiddleWare.cs
@@ -35,7 +35,11 @@ namespace StoreWebApi.MiddleWares
                 var response = _env.IsDevelopment() ?
                     new ApiExceptionResponse(StatusCodes.Status500InternalServerError, ex?.Message, ex?.StackTrace.ToString())
                     : new ApiExceptionResponse(StatusCodes.Status500InternalServerError);
-                var json= JsonSerializer.Serialize(response);
+                var options = new JsonSerializerOptions()
+                {
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                };
+                var json= JsonSerializer.Serialize(response,options);
 
                 await context.Response.WriteAsync(json);
 
