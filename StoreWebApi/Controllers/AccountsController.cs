6dcb30949299e3a3747ea353cf2ba97d5c5425fb
AbdelhamedBelal <abdelhamiedb@gmail.com>
2024-10-22 21:18:51 +0300

    Get Current User

--- StoreWebApi/Controllers/AccountsController.cs
+++ StoreWebApi/Controllers/AccountsController.cs
@@ -1,8 +1,12 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using StoreWeb.Core.Dtos.Auth;
+using StoreWeb.Core.Entities.Identity;
 using StoreWeb.Core.Services.Contract;
+using StoreWeb.Services.Services.Tokens;
 using StoreWebApi.Errors;
+using System.Security.Claims;
 
 namespace StoreWebApi.Controllers
 {
@@ -10,10 +14,14 @@ namespace StoreWebApi.Controllers
     public class AccountsController : BaseApiController
     {
         private readonly IUserService _userService;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly ITokenService _tokenService;
 
-        public AccountsController(IUserService userService)
+        public AccountsController(IUserService userService,UserManager<AppUser> userManager,ITokenService tokenService)
         {
             _userService = userService;
+            _userManager = userManager;
+            _tokenService = tokenService;
         }
 
         [HttpPost("login")]
@@ -36,9 +44,35 @@ namespace StoreWebApi.Controllers
             if (user is null) return BadRequest(new ApiErrorResponse(StatusCodes.Status400BadRequest, "Invalid Registration")); 
 
             return Ok(user);
+             
+        } 
+
+
+
+        [HttpGet("GetCurrentUser")]
+        public async Task <ActionResult<UserDto>> GetCurrentUser()
+        {
+            var userEmail = User.FindFirstValue(ClaimTypes.Email);
+
+            if (userEmail is null) return BadRequest(new ApiErrorResponse(StatusCodes.Status400BadRequest));
+
+            var user= await _userManager.FindByEmailAsync(userEmail);
+
+            if (user is null) return BadRequest(new ApiErrorResponse( StatusCodes.Status400BadRequest));
+            
+            return Ok(new UserDto()
+            {
+                Email = user.Email,
+                DisplayName = user.DisplayName,
+                Token = await _tokenService.CreateTokenAsync(user, _userManager)
+
+            });
 
              
         }
+
+
+
 
 
 
